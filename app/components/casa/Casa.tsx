7612cd42ec062da36a72367b85219e462245fef3
Valeamar <valen.amarasingham@gmail.com>
2023-10-23 19:59:42 -0500

    Actual Casa

---
+++ app/components/casa/Casa.tsx
@@ -0,0 +1,14 @@
+const Casa = () =>{
+    return(
+        <div className="banner">
+            <div className="navbar">
+                <img src = "logo.png" className="logo"></img>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default Casa
