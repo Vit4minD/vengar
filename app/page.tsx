356995fd8f65df79cb2fbbb1ab455e2b847c49a2
Valeamar <valen.amarasingham@gmail.com>
2023-10-23 19:55:03 -0500

    Casa page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-import SignIn from "./components/auth/SignIn"
-import SignUp from "./components/auth/SignUp"
+
 import Firebase from '../firebase/config'
-import AuthDetails from "./components/AuthDetails"
+import Casa from './components/casa/Casa'
 
 const firebase = Firebase
 
 export default function Home() {
   return (
     <>
-      <SignIn />
-      <SignUp />
-      <AuthDetails />
+      <Casa />
     </>
   )
 }
