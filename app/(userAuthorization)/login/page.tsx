059833edde21e647d9a7b5ae8ac840d3bf3d86d8
Vit4minD <108440275+Vit4minD@users.noreply.github.com>
2023-10-26 22:52:23 -0500

    backend Improvement, google sign in, and user redirection

--- app/(userAuthorization)/login/page.tsx
+++ app/(userAuthorization)/login/page.tsx
@@ -1,21 +1,48 @@
 'use client'
 import React, {useState} from 'react'
-import Firebase, {auth} from '../../../firebase/config'
-import { signInWithEmailAndPassword } from 'firebase/auth'
+import Firebase, {auth, db} from '../../../firebase/config'
+import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import AuthDetails from '../components/AuthDetails'
+import { doc, getDoc, setDoc } from 'firebase/firestore'
+import { useRouter } from 'next/navigation'
+
+
 
 const SignIn = () => {
     const[email,setEmail] = useState('')
     const[password,setPassword] = useState('')
-
+    const router = useRouter();
     const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         signInWithEmailAndPassword(auth, email, password)
         .then((userCredential)=> {
             console.log(userCredential)
+            router.push('/questionaire')
         }).catch((error)=>{
-            console.log(error);
+            alert('Invalid Email or Password');
+        })  
+    }
+    const googleAuth = async () =>{
+        const provider = await new GoogleAuthProvider()
+        signInWithPopup(auth, provider).then(async (userCredential)=>
+        {
+          let email:string = userCredential.user.email!==null ? userCredential.user.email : "";
+          const docRef = doc(db,'users',email)
+          const docSnap = await getDoc(docRef)
+          if(docSnap.exists() === false){
+            const data = {
+                email : email,
+                career: null,
+                collegeRigor: null,
+                educationLvl: null,
+                questionaire: false,
+              }
+              await setDoc(doc(db, 'users', email), data)
+          }
+          router.push('/questionaire')
         })
+        .catch((error) => console.log(error));
+        return;
     }
   return (
     <div>
@@ -25,11 +52,11 @@ const SignIn = () => {
             <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
             <button type='submit'>Log In Chat</button>
         </form>
+        <button onClick={googleAuth}>Log In With Google</button>
         <div>Don't have an account? <a href='signUp'>Sign Up</a></div>
     </div>
   )
 }
-
 export default function Home() {
     return(
         <>
--- app/(userAuthorization)/signUp/page.tsx
+++ app/(userAuthorization)/signUp/page.tsx
@@ -1,15 +1,18 @@
 'use client'
 import React, {useState} from 'react'
 import Firebase, {auth} from '../../../firebase/config'
-import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup} from 'firebase/auth'
 import { onAuthStateChanged } from 'firebase/auth';
-import { collection, setDoc, doc } from 'firebase/firestore';
+import { collection, setDoc, doc, getDoc } from 'firebase/firestore';
 import { db } from '../../../firebase/config';
 import AuthDetails from '../components/AuthDetails';
+import { useRouter } from 'next/navigation';
 
 const myCollection = collection(db, 'users');
 
+
 const SignUp = () => {
+    const router = useRouter();
     const[email,setEmail] = useState('')
     const[password,setPassword] = useState('')
     const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
@@ -20,16 +23,43 @@ const SignUp = () => {
                 if (user) {
                   let email:string = user.email!==null ? user.email : "";
                   const data = {
-                    email : user.email
+                    email : user.email,
+                    career: null,
+                    collegeRigor: null,
+                    educationLvl: null,
+                    questionaire: false,
                   }
                   await setDoc(doc(db, 'users', email), data)
                 }
+                router.push('/questionaire')
               });
             console.log(userCredential)
         }).catch((error)=>{
             alert('An account with this email has already been created.')
-        })      
+        })    
     }
+    const googleAuth = async () =>{
+      const provider = await new GoogleAuthProvider()
+      signInWithPopup(auth, provider).then(async (userCredential)=>
+      {
+        let email:string = userCredential.user.email!==null ? userCredential.user.email : "";
+        const docRef = doc(db,'users',email)
+        const docSnap = await getDoc(docRef)
+        if(docSnap.exists() === false){
+          const data = {
+              email : email,
+              career: null,
+              collegeRigor: null,
+              educationLvl: null,
+              questionaire: false,
+            }
+            await setDoc(doc(db, 'users', email), data)
+        }
+        router.push('/questionaire')
+      })
+      .catch((error) => console.log(error));
+      return;
+  }
   return (
     <div>
         <form onSubmit={onSubmit}>
@@ -38,6 +68,7 @@ const SignUp = () => {
             <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
             <button type='submit'>Sign Up</button>
         </form>
+        <button onClick={googleAuth}>Sign Up With Google</button>
         <div>Already have an account? <a href='login'>Log In</a></div>
     </div>
   )
