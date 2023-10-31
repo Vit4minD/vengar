078c51b6b7240c1f12e2f952990953a58b0a6281
Vit4minD <108440275+Vit4minD@users.noreply.github.com>
2023-10-30 21:54:39 -0500

    calendar addEvent

--- app/Calendar/page.tsx
+++ app/Calendar/page.tsx
@@ -141,6 +141,27 @@ export default function Home() {
         )}`;
         todayShowTime.textContent = formateTimer;
       }, 1000);
+      const makeEvent = document.getElementById("makeEvent");
+      const addEvent = document.getElementById("addEvent");
+      const exitEvent = document.getElementById("exitEvent")
+      makeEvent?.addEventListener("click",() =>{
+        if(addEvent!=null){
+            addEvent.style.display = "block";
+            setTimeout(function() {
+                addEvent.style.opacity = '1'; // Fade in the popup
+            }, 10);
+        }
+      });
+      exitEvent?.addEventListener("click",()=>{
+        if(addEvent!=null){
+            addEvent.style.transition = "opacity .3s"
+            addEvent.style.opacity = '0';
+            setTimeout(function() {
+                addEvent.style.display = "none"; // Fade in the popup
+            }, 450);
+            
+        }
+      })
     }, []);
       
   return (
@@ -155,7 +176,7 @@ export default function Home() {
             <link rel ="stylesheet" href = "style.css"></link>
         </head>
         </Head>
-        <body>
+        <body id="exitMakeEvent">
             <div className="banner">
                 <div className="navbar">
                     <img src = "./favicon.ico" className = "logo"></img>
@@ -189,7 +210,7 @@ export default function Home() {
                                 <div>Fri</div>
                                 <div>Sat</div>
                             </div>
-                            <div className="calendar-days">
+                            <div className="calendar-days" id='makeEvent'>
                             </div>
                         </div>
                         <div className="calendar-footer">
@@ -199,6 +220,17 @@ export default function Home() {
                             <div className="date-time-val">
                                 <div className="time-form">09:59</div>
                                 <div className="date-form">October - 24 - 2023</div>
+                            </div>
+                        </div>
+                        <div className='addEvent' id='addEvent'>
+                        <div className='exitEvent'><a  id='exitEvent'>X</a></div>
+                            Add Event  
+                            <div>
+                                <form>
+                                    <p className='date'>Date:<input type="date"></input></p>
+                                    <p className='time'>Time:<input type="time"></input></p>
+                                    <p className='description' >Desc:<input placeholder='Description' type="text"></input></p>
+                                </form>
                             </div>
                         </div>
                         <div className="months-list"></div>
@@ -211,234 +243,3 @@ export default function Home() {
     </>
   )
 }
-
-
-{/* 'use client'
-import { useEffect } from 'react';
-import './style.css';
-import Head from 'next/head';
-import React from 'react';
-export default function Home() {
-//   useEffect(() => {
-//     const isLeapYear = (year: number) => {
-//         return (
-//           (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
-//           (year % 100 === 0 && year % 400 === 0)
-//         );
-//       };
-//       const getFebDays = (year: number) => {
-//         return isLeapYear(year) ? 29 : 28;
-//       };
-//       let calendar = document.querySelector('.calendar');
-//       const month_names = [
-//         'January',
-//         'February',
-//         'March',
-//         'April',
-//         'May',
-//         'June',
-//         'July',
-//         'August',
-//         'September',
-//         'October',
-//         'November',
-//         'December',
-//       ];
-//       let month_picker = document.querySelector('#month-picker');
-//       const dayTextFormate = document.querySelector('.day-text-formate');
-//       const timeFormate = document.querySelector('.time-formate');
-//       const dateFormate = document.querySelector('.date-formate');
-//       if(month_picker!=null)
-//         month_picker.onclick = () => {
-//             month_list.classList.remove('hideonce');
-//             month_list.classList.remove('hide');
-//             month_list.classList.add('show');
-//             dayTextFormate.classList.remove('showtime');
-//             dayTextFormate.classList.add('hidetime');
-//             timeFormate.classList.remove('showtime');
-//             timeFormate.classList.add('hideTime');
-//             dateFormate.classList.remove('showtime');
-//             dateFormate.classList.add('hideTime');
-//         };
-      
-//       const generateCalendar = (month, year) => {
-//         let calendar_days = document.querySelector('.calendar-days');
-//         if(calendar_days !=null)
-//             calendar_days.innerHTML = '';
-//         let calendar_header_year = document.querySelector('#year');
-//         let days_of_month = [
-//           31,
-//           getFebDays(year),
-//           31,
-//           30,
-//           31,
-//           30,
-//           31,
-//           31,
-//           30,
-//           31,
-//           30,
-//           31,
-//         ];
-        
-//         let currentDate = new Date();
-//         if(month_picker)
-//             month_picker.innerHTML = month_names[month];
-//         if(calendar_header_year)
-//             calendar_header_year.innerHTML = year;
-        
-//         let first_day = new Date(year, month);
-      
-      
-//         for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
-       
-//           let day = document.createElement('div');
-      
-//           if (i >= first_day.getDay()) {
-//             day.innerHTML = i - first_day.getDay() + 1;
-    
-//             if (i - first_day.getDay() + 1 === currentDate.getDate() &&
-//               year === currentDate.getFullYear() &&
-//               month === currentDate.getMonth()
-//             ) {
-//               day.classList.add('current-date');
-//             }
-//           }
-//           if(calendar_days)
-//             calendar_days.appendChild(day);
-//         }
-//       };
-      
-//       let month_list = calendar != null ? calendar.querySelector('.month-list') : null;
-//       month_names.forEach((e, index) => {
-//         let month = document.createElement('div');
-//         month.innerHTML = `<div>${e}</div>`;
-      
-//         month_list?.append(month);
-//         month.onclick = () => {
-//           currentMonth.value = index;
-//           generateCalendar(currentMonth.value, currentYear.value);
-//           month_list.classList.replace('show', 'hide');
-//           dayTextFormate.classList.remove('hideTime');
-//           dayTextFormate.classList.add('showtime');
-//           timeFormate.classList.remove('hideTime');
-//           timeFormate.classList.add('showtime');
-//           dateFormate.classList.remove('hideTime');
-//           dateFormate.classList.add('showtime');
-//         };
-//       });
-      
-//       (function () {
-//         if(month_list !=null)
-//             month_list.classList.add('hideonce');
-//       })();
-//       if(document.querySelector('#pre-year') != null)
-//         document.querySelector('#pre-year').onclick = () => {
-//             --currentYear.value;
-//             generateCalendar(currentMonth.value, currentYear.value);
-//         };
-//       if(document.querySelector('#pre-year') != null)
-//         document.querySelector('#next-year').onclick = () => {
-//             ++currentYear.value;
-//             generateCalendar(currentMonth.value, currentYear.value);
-//         };
-      
-//       let currentDate = new Date();
-//       let currentMonth = { value: currentDate.getMonth() };
-//       let currentYear = { value: currentDate.getFullYear() };
-//       generateCalendar(currentMonth.value, currentYear.value);
-    
-//       const todayShowTime = document.querySelector('.time-formate');
-//       const todayShowDate = document.querySelector('.date-formate');
-      
-//       const currshowDate = new Date();
-//       const showCurrentDateOption = {
-//         year: 'numeric',
-//         month: 'long',
-//         day: 'numeric',
-//         weekday: 'long',
-//       };
-//       const currentDateFormate = new Intl.DateTimeFormat(
-//         'en-US',
-//         showCurrentDateOption
-//       ).format(currshowDate);
-//       if(todayShowDate)
-//         todayShowDate.textContent = currentDateFormate;
-//       setInterval(() => {
-//         const timer = new Date();
-//         const option = {
-//           hour: 'numeric',
-//           minute: 'numeric',
-//           second: 'numeric',
-//         };
-//         const formateTimer = new Intl.DateTimeFormat('en-us', option).format(timer);
-//         let time = `${`${timer.getHours()}`.padStart(
-//           2,
-//           '0'
-//         )}:${`${timer.getMinutes()}`.padStart(
-//           2,
-//           '0'
-//         )}: ${`${timer.getSeconds()}`.padStart(2, '0')}`;
-//         if(todayShowTime)
-//             todayShowTime.textContent = formateTimer;
-//       }, 1000);
-//     })
-//   return (
-//     <>
-//         <html lang="en">
-//             <Head>
-//             <head>
-//                 <meta charSet="UTF-8" />
-//                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-//                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//                 <title>CALENDAR</title>
-//                 <link rel="stylesheet" href="style.css"></link>
-//             </head>
-//             </Head>
-//             <body>
-//                 <div className="contianer">
-//                 <div className="calendar">
-//                     <div className="calendar-header">
-//                     <span className="month-picker" id="month-picker"> May </span>
-//                     <div className="year-picker" id="year-picker">
-//                         <span className="year-change" id="pre-year">
-//                         <pre>&lt;</pre>
-//                         </span>
-//                         <span id="year">2020 </span>
-//                         <span className="year-change" id="next-year">
-//                         <pre>&gt;</pre>
-//                         </span>
-//                     </div>
-//                     </div>
-            
-//                     <div className="calendar-body">
-//                     <div className="calendar-week-days">
-//                         <div>Sun</div>
-//                         <div>Mon</div>
-//                         <div>Tue</div>
-//                         <div>Wed</div>
-//                         <div>Thu</div>
-//                         <div>Fri</div>
-//                         <div>Sat</div>
-//                     </div>
-//                     <div className="calendar-days">
-//                     </div>
-//                     </div>
-//                     <div className="calendar-footer">
-//                     </div>
-//                     <div className="date-time-formate">
-//                     <div className="day-text-formate">TODAY</div>
-//                     <div className="date-time-value">
-//                         <div className="time-formate">02:51:20</div>
-//                         <div className="date-formate">23 - july - 2022</div>
-//                     </div>
-//                     </div>
-//                     <div className="month-list"></div>
-//                 </div>
-//                 </div>
-//             </body>
-//         </html>
-//     </>
-//   )
-// }
- */}
